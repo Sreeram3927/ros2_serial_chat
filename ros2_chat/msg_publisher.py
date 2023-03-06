a49913aed557a7f4609b9115676da6e6b51f6551
Sreeram3927 <sreeram292004@gmail.com>
2023-03-06 21:05:29 +0530

    create node and publisher

--- ros2_chat/msg_publisher.py
+++ ros2_chat/msg_publisher.py
@@ -3,3 +3,12 @@ from std_msgs.msg import String
 
 def main(args = None) :
     rclpy.init(args = args)
+    
+    node = rclpy.create_node('publisher_node')
+    publisher = node.create_publisher(String, 'serial_chat', 10)
+    
+    while rclpy.ok():
+        input_msg = input('Enter a message to publish: ')
+        msg = String()
+        msg.data = input_msg
+        publisher.publish(msg)

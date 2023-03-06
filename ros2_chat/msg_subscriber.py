2ae56a1a1a10594c30d220fc3d49142b3f515346
Sreeram3927 <sreeram292004@gmail.com>
2023-03-06 20:17:54 +0530

    create callback function

--- ros2_chat/msg_subscriber.py
+++ ros2_chat/msg_subscriber.py
@@ -1,8 +1,11 @@
 import rclpy
 from std.msg import Strings
 
+def callback(msg) :
+    print(msg)
+
 def main(args = None):
     rclpy.init(args = args)
     
     node = rclpy.create_node('subscriber_node')
-    subscription = node.create_subscription(String, 'serial_chat', , 10)
+    subscription = node.create_subscription(String, 'serial_chat', callback, 10)

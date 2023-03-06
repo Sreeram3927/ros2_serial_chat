4e1227c42df512994cbdfb38f900c8e2b8cd1b7c
Sreeram3927 <sreeram292004@gmail.com>
2023-03-06 20:16:57 +0530

    create node and subscription

--- ros2_chat/msg_subscriber.py
+++ ros2_chat/msg_subscriber.py
@@ -3,3 +3,6 @@ from std.msg import Strings
 
 def main(args = None):
     rclpy.init(args = args)
+    
+    node = rclpy.create_node('subscriber_node')
+    subscription = node.create_subscription(String, 'serial_chat', , 10)

28379233a369f7c5b66ec5767113e03fb9838049
Sreeram3927 <sreeram292004@gmail.com>
2023-03-07 18:24:00 +0530

    create publisher

--- ros2_chat/serial_node.py
+++ ros2_chat/serial_node.py
@@ -4,5 +4,10 @@ from std_msgs.msg import String
 import serial
 
 class SerialNode(Node):
+    
     def __init__(self):
         super().__init__('serial_node')
+
+        self.serial_port = serial.Serial('/dev/ttyACM0', 9600)
+
+        self.publisher_ = self.create_publisher(String, 'serial_data', 10)
